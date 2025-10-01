a466dbd624987faa126d04d77b7afdbde0b2e29c
ksg5647 <isinggeimle@gmail.com>
2025-10-01 09:20:11 +0000

    내 첫번째 앱 배포

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,8 @@ import streamlit as st
 
 st.title("🎈 내 첫번째 앱")
 st.write(
-    "안녕 나는 1-6 정하윤이다. "
+    "지크지온!! "
 
 )
 st.write("")
-st.image("https://i.namu.wiki/i/FB74_pXDdFRmdr2UMwiG1iUj0bvwy6-1Y0au_0CrOY8gm4q6KXukIb8Cw4WTpJxQrIowULWH_AoSfLqeAvkAvw.webp")
+st.image("https://dcimg1.dcinside.com/viewimage.php?id=2aa8de22e4df&no=24b0d769e1d32ca73cea82fa11d0283167b3609c5c7cf1dd93c16921856007a2bdecc452dde28af347349cbb02cebfe57172f35734862761663906816ad39d5eb0344ba4881515fda0600cd77a")
